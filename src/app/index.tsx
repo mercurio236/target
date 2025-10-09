0cca541851de89ff2ea21daed524f19973a43663
Arley Souto <macx15@hotmail.com>
2025-10-09 19:40:52 -0300

    feat: listagem finalizada

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,12 @@
 import { Button } from '@/components/button'
 import { List } from '@/components/list'
-import { Target } from '@/components/target'
-import { StatusBar, View } from 'react-native'
+import { Target, TargetProps } from '@/components/target'
+import { Alert, StatusBar, View } from 'react-native'
 import { HomeHeader } from '../components/home-header'
-import { router } from 'expo-router'
+import { router, useFocusEffect } from 'expo-router'
+import { useTargetDatabase } from '@/database/useTargetDatabase'
+import { useCallback, useState } from 'react'
+import { Loading } from '@/components/loading'
 
 const summary = {
   total: 'R$ 2.680,00',
@@ -11,31 +14,54 @@ const summary = {
   output: { label: 'Saídas', value: '-R$ 883.65' },
 }
 
-const target = [
-  {
-    id: '0',
-    name: 'Comprar uma cadeira ergonômica',
-    current: '900,00',
-    percentage: '75%',
-    target: '1200,00',
-  },
-  {
-    id: '1',
-    name: 'Comprar uma cadeira ergonômica',
-    current: '900,00',
-    percentage: '75%',
-    target: '1200,00',
-  },
-]
-
 export default function Index() {
+  const [isFetching, setIsFetching] = useState(true)
+  const [targets, setTargets] = useState<TargetProps[]>([])
+  const targetDatabase = useTargetDatabase()
+
+  async function fetchTargets(): Promise<TargetProps[]> {
+    try {
+      const response = await targetDatabase.listBySavedValue()
+
+      return response.map((item) => ({
+        id: String(item.id),
+        name: item.name,
+        current: String(item.current),
+        percentage: item.percentage.toFixed(0) + '%',
+        target: String(item.amount),
+      }))
+    } catch (error) {
+      Alert.alert('Erro', 'Não foi possivel carregar as metas.')
+      console.log(error)
+    }
+  }
+
+  async function fetchData() {
+    const targetDataPromise = fetchTargets()
+
+    const [targetData] = await Promise.all([targetDataPromise])
+
+    setTargets(targetData)
+    setIsFetching(false)
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchData()
+    }, [])
+  )
+
+  if (isFetching) {
+    return <Loading />
+  }
+
   return (
     <View style={{ flex: 1 }}>
-      <StatusBar barStyle='light-content'/>
+      <StatusBar barStyle="light-content" />
       <HomeHeader data={summary} />
 
       <List
-        data={target}
+        data={targets}
         renderItem={({ item }) => (
           <Target
             data={item}
--- src/app/target.tsx
+++ src/app/target.tsx
@@ -2,9 +2,46 @@ import { Button } from '@/components/button'
 import { CurrencyInput } from '@/components/currency-input'
 import { Input } from '@/components/input'
 import { PageHeader } from '@/components/page-header'
-import { Text, View } from 'react-native'
+import { useTargetDatabase } from '@/database/useTargetDatabase'
+import { router, useLocalSearchParams } from 'expo-router'
+import { useState } from 'react'
+import { Alert, Text, View } from 'react-native'
 
 export default function Target() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [name, setName] = useState('')
+  const [amount, setAmount] = useState(0)
+
+  const params = useLocalSearchParams<{ id?: string }>()
+  const targetDatabase = useTargetDatabase()
+
+  function handleSave() {
+    if (!name.trim() || amount <= 0) {
+      return Alert.alert('Atenção', 'Preencha o nome e o valor.')
+    }
+
+    if (params.id) {
+    } else {
+      create()
+    }
+  }
+
+  async function create() {
+    try {
+      await targetDatabase.create({ name, amount })
+      Alert.alert('Nova Meta', 'Meta criada com sucesso!', [
+        {
+          text: 'OK',
+          onPress: () => router.back(),
+        },
+      ])
+    } catch (error) {
+      Alert.alert('Erro', 'Não foi possível criar a meta.')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader
@@ -12,9 +49,18 @@ export default function Target() {
         subtitle="A cada valor guardado você fica mais próximo da sua meta. Se esforce para guardar e evitar retirar."
       />
       <View style={{ marginTop: 32, gap: 24 }}>
-        <Input label="Nome da meta" placeholder='Ex: Viagem para praia, Apple Watch'/>
-        <CurrencyInput label='Valor alvo' value={0}/>
-        <Button title="Salvar" />
+        <Input
+          onChangeText={setName}
+          value={name}
+          label="Nome da meta"
+          placeholder="Ex: Viagem para praia, Apple Watch"
+        />
+        <CurrencyInput
+          onChangeValue={setAmount}
+          label="Valor alvo"
+          value={amount}
+        />
+        <Button onPress={handleSave} title="Salvar" isProcessing={isLoading} />
       </View>
     </View>
   )
